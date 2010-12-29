3d74c87e9ee0ab4231731d946749f07c1a5aa7c8
Dan Sully <daniel-github@electricrain.com>
2010-12-28 17:13:51 -0800

    Add /usr/local/lib to library_dirs.

--- setup.py
+++ setup.py
@@ -14,7 +14,9 @@ This is a simple wrapper for some of libFLAC, namely the file decoder, file
 encoder, and metadata interfaces. Most of the functions of these interfaces
 are working (I think).""",
       packages = ["flac"],
-      ext_modules = [Extension('flac/_%s' % i, ['flac/%s_wrap.c' % i],
-                               libraries = ['FLAC']) for i in
-                     ["encoder", "decoder", "sw_metadata"]]
+      ext_modules = [
+        Extension('flac/_%s' % i, ['flac/%s_wrap.c' % i],
+        library_dirs=['/usr/local/lib'],
+        libraries=['FLAC']) for i in ["encoder", "decoder", "sw_metadata"]
+      ]
     )
