fbd7011f19e9bd6a25820f792d83df8734565c9d
Dan Sully <daniel-github@electricrain.com>
2010-12-28 21:38:30 -0800

    Update metadata / README.

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
-setup(name = "pyflac", version = "0.0.4",
-      url="http://sacredchao.net/~piman/software/python.shtml",
-      description = "libFLAC wrapper",
-      author = "David Collett",
-      author_email = "david.collett@dart.net.au",
-      maintainer = "Joe Wreschnig",
-      maintainer_email = "piman@sacredchao.net",
-      license = "GNU GPL v2 or later",
-      long_description = """
+setup(
+    name = "pyflac", version = "0.1",
+    url="https://github.com/dsully/pyflac",
+    description = "libFLAC wrapper",
+    author = "David Collett, Joe Wreschnig, Dan Sully",
+    author_email = "david.collett@dart.net.au",
+    maintainer = "Dan Sully",
+    license = "GNU GPL v2 or later",
+    long_description = """
 This is a simple wrapper for some of libFLAC, namely the file decoder, file
 encoder, and metadata interfaces. Most of the functions of these interfaces
 are working (I think).""",
-      packages = ["flac"],
-      ext_modules = [
+
+    packages = ["flac"],
+    ext_modules = [
         Extension('flac/_%s' % i, ['flac/%s_wrap.c' % i],
         library_dirs=['/usr/local/lib'],
         libraries=['FLAC']) for i in ["encoder", "decoder", "sw_metadata"]
-      ]
-    )
+    ]
+)
